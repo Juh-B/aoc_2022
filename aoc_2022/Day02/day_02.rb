c4ae63d63a6b4b0cc7b873595cfaa519594ab17e
Juliana Bonardi <julicbonardi@hotmail.com>
2022-12-09 14:28:34 -0300

    day 02 - part 02

--- aoc_2022/Day02/day_02.rb
+++ aoc_2022/Day02/day_02.rb
@@ -8,10 +8,11 @@ class Day02
 
   # part 02 -> x lose = 0 | Y draw = 3 | Z win = 6
 
-  score = 0
+  score_part01 = 0
+  score_part02 = 0
 
   # first_col = my asnwer (X, Y, Z) | sec_col = outcome
-  game = {
+  game_part01 = {
     'A X': 1 + 3,
     'A Y': 2 + 6,
     'A Z': 3 + 0,
@@ -23,9 +24,27 @@ class Day02
     'C Z': 3 + 3
   }
 
+  # first_col = my asnwer | sec_col = outcome (X, Y,  Z)
+  game_part02 = {
+    'A X': 3 + 0,
+    'A Y': 1 + 3,
+    'A Z': 2 + 6,
+
+    'B X': 1 + 0,
+    'B Y': 2 + 3,
+    'B Z': 3 + 6,
+
+    'C X': 2 + 0,
+    'C Y': 3 + 3,
+    'C Z': 1 + 6
+  }
+
   file.each do |line|
-    score += game[:"#{line.chomp}"]
+    score_part01 += game_part01[:"#{line.chomp}"]
+    score_part02 += game_part02[:"#{line.chomp}"]
   end
 
-  puts "result Part 01 - #{score}"
+  puts "result Part 01 - #{score_part01}"
+  puts "----------------"
+  puts "result Part 02 - #{score_part02}"
 end

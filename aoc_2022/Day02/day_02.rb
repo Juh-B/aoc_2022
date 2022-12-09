b6e6c429c263f894d3984d6cada65425a13e4cf9
Juliana Bonardi <julicbonardi@hotmail.com>
2022-12-09 14:22:13 -0300

    day 02 - part 01

--- aoc_2022/Day02/day_02.rb
+++ aoc_2022/Day02/day_02.rb
@@ -1,4 +1,31 @@
 class Day02
-  file = File.readlines('./Day02/sample.txt')
+  file = File.readlines('./Day02/input.txt')
 
+  # scissor > paper > rock > scissor
+  # A rock = 1 | B paper = 2 | C scissor = 3
+  # X rock = 1 | Y paper = 2 | Z scissor = 3
+  # outcome -> lost 0, draw 3, won 6
+
+  # part 02 -> x lose = 0 | Y draw = 3 | Z win = 6
+
+  score = 0
+
+  # first_col = my asnwer (X, Y, Z) | sec_col = outcome
+  game = {
+    'A X': 1 + 3,
+    'A Y': 2 + 6,
+    'A Z': 3 + 0,
+    'B X': 1 + 0,
+    'B Y': 2 + 3,
+    'B Z': 3 + 6,
+    'C X': 1 + 6,
+    'C Y': 2 + 0,
+    'C Z': 3 + 3
+  }
+
+  file.each do |line|
+    score += game[:"#{line.chomp}"]
+  end
+
+  puts "result Part 01 - #{score}"
 end

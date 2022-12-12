bc9a8bf986facfde6963a0dee0e0c52ae9298293
Juliana Bonardi <julicbonardi@hotmail.com>
2022-12-12 16:57:22 -0300

    result part 02 - day 03

--- aoc_2022/Day03/day_03.rb
+++ aoc_2022/Day03/day_03.rb
@@ -1,19 +1,34 @@
 class Day03
   file = File.readlines('./Day03/input.txt')
+  rucksacks = file.map(&:chomp).map(&:chars)
 
-  rucksack = file.map(&:chomp).map(&:chars)
-  total = 0
+  # Part 01
+  total_01 = 0
 
-  rucksack.each do |compartment|
+  rucksacks.each do |compartment|
     half = compartment.length/2
     rigth = compartment.first(half)
     left = compartment.last(half)
 
     itens = rigth.intersection(left)
     itens.each do |item|
-      total += item =~ /[a-z]/ ? item.ord - 96 : item.ord - 38
+      total_01 += item =~ /[a-z]/ ? item.ord - 96 : item.ord - 38
     end
   end
 
-  puts "The sum of the priorities is #{total}"
+  puts "Part 01 -> The sum of the priorities is #{total_01}"
+
+
+  # Part 02
+  total_02 = 0
+
+  rucksacks.each_slice(3).to_a.each do |rucksack|
+    badges = rucksack[0].intersection(rucksack[1], rucksack[2])
+    badges.each do |sticker|
+      total_02 += sticker =~ /[a-z]/ ? sticker.ord - 96 : sticker.ord - 38
+    end
+  end
+
+  puts "Part 02 -> The sum of the priorities is #{total_02}"
+
 end

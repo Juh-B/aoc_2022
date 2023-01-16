ea4ab0783b98dc350f2488ef44f99145fda8c32b
Juliana Bonardi <julicbonardi@hotmail.com>
2023-01-16 19:29:19 -0300

    Day 04 part 02 complet

--- aoc_2022/Day04/day_04.rb
+++ aoc_2022/Day04/day_04.rb
@@ -6,9 +6,21 @@ class Day04
   array = []
   file.each { |l| l.chomp.split(/[,-]/).each { |c| array << c.to_i if c.ord > 47 } }
 
-  p array
+  # Part 01
+  part01 = array
     .each_slice(2).to_a
     .map { |a, b| (a..b).to_set }
     .each_slice(2).to_a
     .reduce(0) { |count, (a, b)| a.subset?(b) || b.subset?(a) ? count + 1 : count }
+
+  puts "Part 01 -> One range fully contain the other in #{part01} assignment pairs "
+
+  # Part 02
+  part02 = array
+    .each_slice(2).to_a
+    .map { |a, b| (a..b).to_set }
+    .each_slice(2).to_a
+    .reduce(0) { |count, (a, b)| a.intersect?(b) ? count + 1 : count }
+
+  puts "Part 02 -> The ranges overlap in #{part02} assignment pairs "
 end
